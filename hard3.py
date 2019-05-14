4b9e121a89fbdf3fcc6bc7578494e87e9907c5f6
Georgy Dvoryak <gosha222222@users.noreply.github.com>
2019-05-14 15:45:02 +0300

    changed + trash deleted

--- hard3.py
+++ hard3.py
@@ -1,3 +1,5 @@
+# Программа случайно рассчитывает урон для каждого удара.
+# Умеет записать данные в файл, но не умеет считать.
 import random
 
 def attack(u1, u2):
@@ -20,26 +22,14 @@ def armor(u1, u2):
     u2["damage"] = u2["damage"] / u1["armor"]
     return u1, u2
 
-def write_to(u1, u2):
-    file = open('name.txt', 'w', encoding='utf-8')
-    str1 = str(u1)
-    str2 = str(u2)
-    file.write(str1 + "\n" + str2)
-    file.close()
+def write_to(player_dict):
 
-def read_from(u1, u2):
-    file = open('name.txt','r', encoding='utf-8')
-    i = 0
-    while i < 2:
-        line = file.readline()
-        line = line.replace("'", "")
-        line = line.replace(" ", "")
-        line = line.replace("{", "")
-        line = line.replace("}", "")
-        line = line.replace("\n", "")
-        line = line.split(',')
-        print(line)
-        i += 1
+    with open(f'{player_dict["name"]}.txt', 'w', encoding='utf-8') as file:
+        for key, value in player_dict.items():
+            if key == "armor":
+                file.write(str(value))
+            else:
+                file.write(str(value) + '; ')
 
 dmg1 = 0
 dmg2 = 0
@@ -50,10 +40,10 @@ name2 = input("Игрок 2, введите имя своего персонаж
 u1 = {"name":name1, "health":100, "damage":dmg1, "armor":1.2}
 u2 = {"name":name2, "health":100, "damage":dmg2, "armor":1.2}
 
-write_to(u1, u2)
+write_to(u1)
+write_to(u2)
 i = 0
 while u1["health"] > 0 and u2["health"] > 0:
     i += 1
     attack(u1, u2)
 winner(u1, u2)
-read_from(u1, u2)
